642c7c08ce6746a5abdbcfc5024b58240227bc67
100milliongold <gadian88@gmail.com>
2020-12-22 07:49:11 +0900

    형태소분석 모듈적용중

---
+++ src/utils/koalanlp/index.ts
@@ -0,0 +1,7 @@
+const { KMR, KKMA } = require('koalanlp/API')
+const { initialize } = require('koalanlp/Util')
+const { Tagger, Parser } = require('koalanlp/proc')
+
+const koalanlp = async (test: any) => {}
+
+export default koalanlp
