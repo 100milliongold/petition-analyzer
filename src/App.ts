67521978f728262a6dbf55201e5d24d60df61ca9
100milliongold <gadian88@gmail.com>
2020-12-28 07:11:48 +0900

    logging

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import morgan from 'morgan'
 
 import routes from 'routes'
 
@@ -17,6 +18,14 @@ class App {
 
   constructor() {
     this.app = express()
+    // log only 4xx and 5xx responses to console
+    this.app.use(
+      morgan('dev', {
+        skip: function (req, res) {
+          return res.statusCode < 400
+        },
+      })
+    )
     this.app.use('/', routes)
   }
 }
