2c5afca5df53aadff267de30493d8ad7bf26e9ef
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-04-09 10:02:55 +0900

    수집툴

--- scraper/src/App.ts
+++ scraper/src/App.ts
@@ -6,90 +6,159 @@ import dateFormat from "dateformat";
 
 import { config } from 'dotenv'
 
+import mysql from "mysql";
+
+import { schedule } from "node-cron";
 
 const Strict = require("fxjs/Strict");
 const S = Strict
 const L = require("fxjs/Lazy");
 const C = require("fxjs/Concurrency");
 
-const { go, delay, each , log, tap, map } = Strict
+const { go, pipe, delay, each , log, tap, map } = Strict
 
 const day_length = 1
 
 const delay_count = 1000 * 3
 
 
-var presto = require('presto-client');
-var client = new presto.Client({user: 'myname', port : 8080 , host: '192.168.0.14'});
-
 /**
  * env
  */
- config();
+config();
 
+/**
+ * 시간 설정
+ */
 const find_data = new Date();
-find_data.setDate( find_data.getDate() + 30 - 1 - day_length);
 find_data.setHours(0)
+find_data.setDate( find_data.getDate() -2 )
 find_data.setMinutes(0)
 find_data.setSeconds(0)
 find_data.setMilliseconds(0)
 console.log(find_data);
 
-const query = (query :string) => new Promise((resolve, reject) => client.execute({
-    query:   query,
-    catalog: 'hive',
-    schema:  'default',
-    // state:   function(error: any, query_id: any, stats: any){ console.log({message:"status changed", id:query_id, stats:stats}); },
-    // columns: function(error: any, data: any){ console.log({resultColumns: data}); },
-    // data:    function(error: any, data: any, columns: any, stats: any){ console.log(data); },
-    success: function(error: any, stats: any){ return resolve(stats); },
-    error:   function(error: any){ reject(error) }
+/**
+ * mysql 접속정보
+ */
+const connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    port     : process.env.PORT ? parseInt(process.env.PORT) : undefined,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : 'petitions'
+});
+connection.connect();
+
+
+/**
+ * maria DB 으로 쿼리문 전송
+ * @param query 쿼리문
+ * @returns 
+ */
+const query = (query : string) : any => new Promise((resolve , reject) => connection.query(query, (error, rows : any, fields : any) => {
+    if (error) reject(error);
+    resolve({rows , fields })
 }))
 
+/**
+ * prepare statement
+ * @param query 
+ * @param values 
+ * @returns 
+ */
+const inSertquery = (query : string, values: string[]) : Promise<any> => new Promise((resolve , reject) => connection.query(query, values , (error, rows : any, fields : any) => {
+    if (error) reject(error);
+    resolve({rows , fields })
+}))
+
+/**
+ * Each 문으로 Insert
+ */
+const eachInsert = pipe(
+    each(({
+        begin,
+        category,
+        content,
+        crawled_at,
+        end,
+        num_agree,
+        status,
+        title,
+        petition_idx,
+
+    } : PETION) => {
+        const inertString : string = `('${dateFormat(begin , "yyyy-mm-dd")}', '${category}', '${content}','${dateFormat(crawled_at , "yyyy-mm-dd")}', '${dateFormat(end , "yyyy-mm-dd")}', ${num_agree}, '${status}', '${title}', ${petition_idx} )`;
+        const values : string[] = [
+            `${dateFormat(begin , "yyyy-mm-dd")}`,
+            `${category}`,
+            `${content}`,
+            `${dateFormat(crawled_at , "yyyy-mm-dd")}`,
+            `${dateFormat(end , "yyyy-mm-dd")}`, 
+            `${num_agree}`,
+            `${status}`,
+            `${title}`, 
+            `${petition_idx}`
+        ]
+        const query = `INSERT INTO petitions (\`begin\`, \`category\`, \`content\`, \`crawled_at\`, \`end\`, \`num_agree\`, \`status\`, \`title\`, \`petition_idx\` ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`
+        inSertquery(query , values).then(({rows}) => console.log(rows))
+    })
+)
 
 
-getFirstIndex().then( async (index : number) => {
-   
-    await go(
-        L.interval(delay_count),
-        L.map((o : number) => index - o),
-        L.map((o : number) => scraper(o)),
-        L.takeUntil( (o : PETION) => o.petition_idx && o.petition_idx < 597213),
-        //L.take(5),
-        map(({
-                begin,
-                category,
-                content,
-                crawled_at,
-                end,
-                num_agree,
-                status,
-                title,
-                petition_idx
-            } : PETION) => `INSERT INTO petitions ("begin", "category", "content", "crawled_at", "end", "num_agree", "status", "title", "petition_idx" ) VALUES ('${dateFormat(begin , "yyyy-mm-dd")}', '${category}', '${content}','${dateFormat(crawled_at , "yyyy-mm-dd")}', '${dateFormat(end , "yyyy-mm-dd")}', ${num_agree}, '${status}', '${title}', ${petition_idx})`
-        ),
-        // map(query),
-        each(log)
-        // each(({
-        //     begin,
-        //     category,
-        //     content,
-        //     crawled_at,
-        //     end,
-        //     num_agree,
-        //     status,
-        //     title,
-        //     petition_idx
-        // } : PETION) => {
-        //     console.log({
-        //         title, end, petition_idx
-        //     });
-        //     const query = `INSERT INFO petitions (begin, category, content, crawled_at, end, num_agree, status, title, petition_idx ) VALUES ('${begin}', '${category}', '${content}','${crawled_at}', '${end}', '${num_agree}', '${status}', '${title}', '${petition_idx}')`
-           
-        // }),
-    )
+/**
+ * 시작번호를 기준으로 3초마다 한번씩 가져오는 메서드
+ * @param start 시작 게시판 번호
+ * @returns 
+ */
+const scrapPetitions = async (start : number) => await go(
+    /**
+     * 3초마다 1씩증가
+     */
+    L.interval(delay_count),
+    /**
+     * -1 씩감소
+     */
+    L.map((o : number) => start - o),
+    /**
+     * 글번호을 토대로 게시판 글 수집
+     */
+    L.map((o : number) => scraper(o)),
+    // L.take(5),
 
-    // console.log(list);
-    
+    /**
+     * 게시날짜와 비교해서 기준치보다 작을경우 중단
+     */
+    L.takeUntil( (o : PETION) => o.begin && o.begin.getTime() <= find_data.getTime()),
+)
 
+/**
+ * 전체 작업 내용 
+ * @returns 
+ */
+const job = () => go(
+    // 1. 게시판을 불려와서 첫번째 글번호를 추측하고
+    getFirstIndex(),
+    // 2. 해당 첫번째 글번호를 토대로 3초당 -1 씩 감소해서 수집
+    scrapPetitions,
+    // 3. 3초마다 수집된 글은 디비에 삽입 
+    eachInsert,
+)
+
+/**
+ * 
+ * cron job
+    +-------------------- second (0 - 59)
+    |  +----------------- minute (0 - 59)
+    |  |  +-------------- hour (0 - 23)
+    |  |  |  +----------- day of month (1 - 31)
+    |  |  |  |  +-------- month (1 - 12)
+    |  |  |  |  |  +----- day of week (0 - 6) (Sunday=0 or 7)
+    |  |  |  |  |  |  +-- year [optional]
+    |  |  |  |  |  |  |
+    *  *  *  *  *  *  * command to be executed 
+ */
+schedule('0 0 * * *' , () => {
+    job()
 })
+
